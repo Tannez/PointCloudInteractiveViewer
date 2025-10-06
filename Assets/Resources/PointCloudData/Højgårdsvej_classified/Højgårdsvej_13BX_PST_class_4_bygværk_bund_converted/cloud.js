14c9903a2c283c6d2eb45de393ebf8593333cf6f
Tannez <92729005+Tannez@users.noreply.github.com>
2025-10-06 14:14:28 +0200

    Manually sey offset for clouds in Class_Based_PointCloud

---
+++ Assets/Resources/PointCloudData/Højgårdsvej_classified/Højgårdsvej_13BX_PST_class_4_bygværk_bund_converted/cloud.js
@@ -0,0 +1,76 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 821932,
+    "offset": [709552.7109,6191769.1994,40.1251], 
+    "boundingBox": {
+        "lx": 0,
+        "ly": 0,
+        "lz": 0,
+        "ux": 3.8937,
+        "uy": 3.8937,
+        "uz": 3.8937
+    },
+    "tightBoundingBox": {
+        "lx": 0,
+        "ly": 0,
+        "lz": 0,
+        "ux": 3.8937,
+        "uy": 3.8937,
+        "uz": 3.8937
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.03372042998671532,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
