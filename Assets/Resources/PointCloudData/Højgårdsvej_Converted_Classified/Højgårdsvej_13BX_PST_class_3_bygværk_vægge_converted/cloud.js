6e9086e9d76fd305a374a2a9a976e0645dc60542
Tannez <92729005+Tannez@users.noreply.github.com>
2025-10-06 10:35:16 +0200

    Reuploaded Class version due to conversion errors

---
+++ Assets/Resources/PointCloudData/Højgårdsvej_Converted_Classified/Højgårdsvej_13BX_PST_class_3_bygværk_vægge_converted/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3548415,
+    "boundingBox": {
+        "lx": 709552.7033,
+        "ly": 6191769.193,
+        "lz": 40.3514,
+        "ux": 709556.8655000002,
+        "uy": 6191773.3552,
+        "uz": 44.51360000013709
+    },
+    "tightBoundingBox": {
+        "lx": 709552.7033,
+        "ly": 6191769.193,
+        "lz": 40.3514,
+        "ux": 709555.8097,
+        "uy": 6191773.3552,
+        "uz": 44.3827
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.036045707762241366,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
