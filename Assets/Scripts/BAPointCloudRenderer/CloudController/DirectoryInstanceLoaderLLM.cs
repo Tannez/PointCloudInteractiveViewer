dc103176172bdf1e5644a6f9358a8ae1e9103179
Tannez <92729005+Tannez@users.noreply.github.com>
2025-11-06 19:23:36 +0100

    New Scene with LLM Menu Only

---
+++ Assets/Scripts/BAPointCloudRenderer/CloudController/DirectoryInstanceLoaderLLM.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BAPointCloudRenderer.CloudData;
+using BAPointCloudRenderer.ObjectCreation;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BAPointCloudRenderer.CloudController {
+    /// <summary>
+    /// Use this loader, if you have several pointcloud-folders in the same directory and want to import all of them to your program.
+    /// To  import them, create a DirectoryLoader and press the "Load Directory" Button in the Editor (or call the function LoadAll).
+    /// 
+    /// Streaming Assets support provided by Pablo Vidaurre
+    /// </summary>
+    [ExecuteInEditMode]
+    public class DirectoryInstanceLoaderLLM : MonoBehaviour {
+
+        /// <summary>
+        /// Path of the directory containing the point clouds
+        /// </summary>
+        public string path;
+        /// <summary>
+        /// When true, the cloudPath is relative to the streaming assets directory
+        /// </summary>
+        public bool streamingAssetsAsRoot = false;
+
+        /// <summary>
+        /// The PointSetController
+        /// </summary>
+        public AbstractPointCloudSet pointset;
+
+        //may include streaming assets path
+        private string fullPath;
+
+        // Count Cloud Loaders Created for Cloud Instantiator <- Added
+        [HideInInspector] public int cloudsInDirectory = 0;
+        [HideInInspector] public int cloudClassesInDirectory = 0;
+        [HideInInspector] public int cloudInstanceInClass = 0;
+
+        // Compare name variables
+        private string currentInstanceClassName;
+        private string previousInstanceClassName;
+
+        [SerializeField] GameObject CloudLoaderPrefab;
+        [SerializeField] GameObject MeshConfigPrefab;
+
+        [SerializeField] public List<Vector3> spawnPositions = new List<Vector3>();
+
+        // Save All Instances in list
+        public List<GameObject> allInstances = new List<GameObject>();
+
+        // Pointcloud Instances in Class
+        [Serializable]
+        public class PCInstances
+        {
+            public GameObject cloudClassGO;
+            public GameObject getCloudInstanceGO;
+            public GameObject getCloudLoaderGO;
+            // public PointCloudLoader getLoader;
+            public GameObject getDynamicLoaderGO;
+
+            public GameObject getMeshConfigGO;
+
+            public PCInstances(GameObject aCloudClassGO, GameObject aCloudInstanceGO, GameObject aCloudLoaderGO, GameObject aDynamicLoaderGO, GameObject aMeshConfigGO)
+            {
+                // Get Cloud Instance GO's for PC manipulation
+                getCloudInstanceGO = aCloudInstanceGO;
+                getCloudLoaderGO = aCloudLoaderGO;
+                getDynamicLoaderGO = aDynamicLoaderGO;
+                getMeshConfigGO = aMeshConfigGO;
+
+                // Make Cloud Instance GO parent of Loader GO's 
+                getCloudLoaderGO.transform.SetParent(getCloudInstanceGO.transform);
+                getDynamicLoaderGO.transform.SetParent(getCloudInstanceGO.transform);
+                getMeshConfigGO.transform.SetParent(getCloudInstanceGO.transform);
+
+                // Make Cloud Instance GO's part of Cloud Class GO
+                cloudClassGO = aCloudClassGO;
+                getCloudInstanceGO.transform.SetParent(cloudClassGO.transform);
+
+                // Add Cloud Interaction Script to each instance
+                getCloudInstanceGO.AddComponent<MouseClickClouds>();
+                getCloudInstanceGO.AddComponent<BoxCollider>();
+                getCloudInstanceGO.tag = "PointCloud";
+            }
+        }
+
+        // List of Cloud Classes
+        public List<PCInstances> pointCloudClasses = new List<PCInstances>(); 
+        
+        GameObject cloudClassGO;
+        PCInstances pCInstances;
+
+        /// <summary>
+        /// Creates PointCloudLoader objects for all the point clouds in the given path.
+        /// </summary>
+        public void LoadAll()
+        {
+            if (streamingAssetsAsRoot) fullPath = Application.streamingAssetsPath + "/" + path;
+            else { fullPath = path; }
+
+            DirectoryInfo dir = new DirectoryInfo(fullPath);
+            // Debug.Log("Entering Cloud Loading");
+
+            cloudInstanceInClass = 0;
+
+            foreach (DirectoryInfo sub in dir.GetDirectories())
+            {
+                // Define first few letters of file name - used to check if instance belongs to same class
+                currentInstanceClassName = sub.Name.Substring(0, 2);
+
+                if (currentInstanceClassName != previousInstanceClassName)
+                {
+                    // Create new class object
+                    //Debug.Log("Creating a New Class: " + "Class: " + (cloudClassesInDirectory+1));
+                    cloudClassesInDirectory++;
+                    cloudClassGO = new GameObject("Class: " + cloudClassesInDirectory);
+                    //cloudClassGO.gameObject.GetComponent<BoxCollider>().size = boundingBox.GetBoundsObject().size;
+                    cloudInstanceInClass = 0;
+                    //Debug.Log("Resetting Instances in Class");
+                }
+
+                cloudInstanceInClass++;
+                //Debug.Log("Current Cloud sub name class: " + currentInstanceClassName);
+
+                //Debug.Log("Loading Current Cloud Instance(" + (cloudsInDirectory + 1) + ") For Class: " + (cloudClassesInDirectory+1));
+
+                // Create Cloud Instance From Directory
+                GameObject cloudInstanceGO = new GameObject("Cloud: " + cloudInstanceInClass);
+                //Debug.Log("Created a cloudInstanceGO numbered: " + cloudInstanceInClass);
+                GameObject go = new GameObject(sub.Name);
+                PointCloudLoader loader = go.AddComponent<PointCloudLoader>();
+                GameObject dynamicLoader;
+                GameObject meshConfigure;
+
+                // Spawn at specific positions if defined in Inspector
+                if (cloudsInDirectory < spawnPositions.Count)
+                {
+                    dynamicLoader = Instantiate(CloudLoaderPrefab, spawnPositions[cloudsInDirectory], Quaternion.identity);
+                    meshConfigure = Instantiate(MeshConfigPrefab);
+                }
+                else
+                {
+                    dynamicLoader = Instantiate(CloudLoaderPrefab);
+                    meshConfigure = Instantiate(MeshConfigPrefab);
+                }
+
+                // Connect Values to Components
+                if (streamingAssetsAsRoot)
+                {
+                    loader.streamingAssetsAsRoot = true;
+                    loader.cloudPath = path + sub.Name;
+                }
+                else
+                {
+                    loader.cloudPath = sub.FullName;
+                }
+
+                loader.setController = dynamicLoader.GetComponent<DynamicPointCloudSet>();
+                meshConfigure.GetComponent<DefaultMeshConfiguration>().renderCamera = Camera.main;
+                dynamicLoader.GetComponent<DynamicPointCloudSet>().meshConfiguration = meshConfigure.GetComponent<DefaultMeshConfiguration>();
+                dynamicLoader.GetComponent<DynamicPointCloudSet>().userCamera = Camera.main;
+
+
+                if (currentInstanceClassName != previousInstanceClassName)
+                {
+                    // Add to New PointCloudClass 
+                
+                    // Create Cloud Instance and add to new Class
+                    pCInstances = new PCInstances(cloudClassGO, cloudInstanceGO, go, dynamicLoader, meshConfigure);
+                    allInstances.Add(pCInstances.getCloudInstanceGO);
+                    //Debug.Log("Instance Added. Total Instances = " + allInstances.Count);
+                    pointCloudClasses.Add(pCInstances);
+
+                    //Debug.Log("Created New Class: " + cloudClassesInDirectory);
+                }
+
+                else if (currentInstanceClassName == previousInstanceClassName)
+                {
+                    // Add to current PointCloudClasses element
+
+                    // Create Cloud Instance and add to same Class
+                    pCInstances = new PCInstances(cloudClassGO, cloudInstanceGO, go, dynamicLoader, meshConfigure);
+                    allInstances.Add(pCInstances.getCloudInstanceGO);  
+                    //Debug.Log("Instance Added. Total Instances = " + allInstances.Count);
+
+                    //Debug.Log("Appending PointCloud to Same Class");
+                }
+
+                previousInstanceClassName = currentInstanceClassName;
+                //Debug.Log("Setting previous Cloud sub name class as: " + previousInstanceClassName);
+                //Debug.Log("Current Amount Of Point Cloud Classes: " + pointCloudClasses.Count);
+                cloudsInDirectory++;
+            }
+
+            // Debug.Log("Clouds Loaded: " + cloudsInDirectory);
+        }
+    }
+}
+
---
+++ Assets/Scripts/LLM/MouseClickClouds.cs
@@ -0,0 +1,216 @@
+using BAPointCloudRenderer.CloudController;
+using UnityEngine;
+using System.Collections.Generic;
+using BAPointCloudRenderer.ObjectCreation;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using System.Linq;
+
+public class MouseClickClouds : MonoBehaviour
+{
+    CloudControllerLLM cloudControllerLLM;
+    public List<DirectoryInstanceLoader.PCInstances> pClasses = new List<DirectoryInstanceLoader.PCInstances>();
+    Ray ray;
+    bool classSelectedwithMouse = false;
+    bool InstanceSelectedwithMouse = false;
+
+    List<int> selectedClasses = new List<int>();
+    List<int> selectedInstances = new List<int>();
+    int displayedClassSelection;
+    int displayedInstanceSelection; 
+
+    void Start()
+    {
+        cloudControllerLLM = gameObject.GetComponent<CloudControllerLLM>();
+        pClasses = cloudControllerLLM.PCClasses;
+    }
+
+    void Update()
+    {
+        CloudClickSelection();
+    }
+
+    // First attempt to get mouse click to work with bounds. Only worked on first instance
+    // private void CloudClickSelectionSingleInstance()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //     var renderer = gameObject.GetComponentInChildren<BAPointCloudRenderer.CloudController.AbstractPointCloudSet>();
+    //     Bounds bounds = renderer.GetTightBoundingBoxBounds();
+
+    //     if (bounds.IntersectRay(ray))
+    //     {
+    //             Debug.Log("Clicked on " + renderer.name);
+    //             Debug.Log("Clicked on " + renderer.gameObject.transform.parent.name);
+    //     }
+    //     }
+    // }
+
+    private void CloudClickSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectedClasses = new List<int>();
+            selectedInstances = new List<int>();
+
+            if (IsPointerOverUI())
+            {
+                //Debug.Log("Mouse over UI");
+                return;
+            }
+
+            else
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                foreach (DirectoryInstanceLoader.PCInstances clouds in pClasses)
+                {
+                    for (int i = 0; i < clouds.cloudClassGO.transform.childCount; i++)
+                    {
+                        GameObject instanceInClass = clouds.cloudClassGO.transform.GetChild(i).gameObject;
+                        var renderer = instanceInClass.GetComponentInChildren<AbstractPointCloudSet>();
+                        Bounds b = renderer.GetTightBoundingBoxBounds();
+
+                        // Convert to world space so that each bounding box is tested in world coordinates,
+                        // matching where the clouds actually are in the scene.
+                        b.center = renderer.transform.TransformPoint(b.center);
+                        b.extents = Vector3.Scale(b.extents, renderer.transform.lossyScale);
+
+                        if (b.IntersectRay(ray))
+                        {
+                            string clickClassTarget = renderer.gameObject.transform.parent.transform.parent.name;
+                            string clickInstanceTarget = renderer.gameObject.transform.parent.name;
+
+                            
+                            if (clickClassTarget == "Class: 1" && classSelectedwithMouse == false && cloudControllerLLM.classHidden[0] == false)
+                            {
+                                selectedClasses.Add(1);
+                                classSelectedwithMouse = true;
+                                //Debug.Log("Added Class: 1");
+                            }
+                            else if (clickClassTarget == "Class: 2" && classSelectedwithMouse == false && cloudControllerLLM.classHidden[1] == false)
+                            {
+                                selectedClasses.Add(2);
+                                classSelectedwithMouse = true;
+                                //Debug.Log("Added Class: 2");
+                            }
+                            else if (clickClassTarget == "Class: 3" && classSelectedwithMouse == false && cloudControllerLLM.classHidden[2] == false)
+                            {
+                                selectedClasses.Add(3);
+                                classSelectedwithMouse = true;
+                                //Debug.Log("Added Class: 3");
+                            }
+                            else if (clickClassTarget == "Class: 4" && classSelectedwithMouse == false && cloudControllerLLM.classHidden[3] == false)
+                            {
+                                selectedClasses.Add(4);
+                                classSelectedwithMouse = true;
+                                //Debug.Log("Added Class: 4");
+                            }
+                            else if (clickClassTarget == "Class: 5" && classSelectedwithMouse == false && cloudControllerLLM.classHidden[4] == false)
+                            {
+                                selectedClasses.Add(5);
+                                classSelectedwithMouse = true;
+                                //Debug.Log("Added Class: 5");
+                            }
+                            
+
+                            if (clickInstanceTarget == "Cloud: 1" && InstanceSelectedwithMouse == false)
+                            {
+                                selectedInstances.Add(1);
+                                InstanceSelectedwithMouse = true;
+                                //Debug.Log("Added Cloud: 1");
+                            }
+                            else if (clickInstanceTarget == "Cloud: 2" && InstanceSelectedwithMouse == false)
+                            {
+                                selectedInstances.Add(2);
+                                InstanceSelectedwithMouse = true;
+                                //Debug.Log("Added Cloud: 2");
+                            }
+                            else if (clickInstanceTarget == "Cloud: 3" && InstanceSelectedwithMouse == false)
+                            {
+                                selectedInstances.Add(3);
+                                InstanceSelectedwithMouse = true;
+                                //Debug.Log("Added Cloud: 3");
+                            }
+                            else if (clickInstanceTarget == "Cloud: 4" && InstanceSelectedwithMouse == false)
+                            {
+                                selectedInstances.Add(4);
+                                InstanceSelectedwithMouse = true;
+                                //Debug.Log("Added Cloud: 4");
+                            }
+                            else if (clickInstanceTarget == "Cloud: 5" && InstanceSelectedwithMouse == false)
+                            {
+                                selectedInstances.Add(5);
+                                InstanceSelectedwithMouse = true;
+                                //Debug.Log("Added Cloud: 5");
+                            }
+
+                            classSelectedwithMouse = false;
+                            InstanceSelectedwithMouse = false;
+                        }
+                    }
+                }
+
+                int classesSelected = selectedClasses.Count;
+                int instancesSelected = selectedInstances.Count;
+
+                if (classesSelected > 0 && instancesSelected > 0) // Must have a selected target
+                {
+                    displayedClassSelection = selectedClasses[classesSelected - 1];
+                    displayedInstanceSelection = selectedInstances[instancesSelected - 1];
+
+                    //Debug.Log("Displayed Class: " + displayedClassSelection);
+                    //Debug.Log("Displayed Class: " + displayedInstanceSelection);
+                    ShowClickedCloud(displayedClassSelection, displayedInstanceSelection);
+                }
+                // else if (classesSelected >= 0 && instancesSelected > 0) // Must have a selected target
+                // {
+                //     displayedInstanceSelection = selectedInstances[instancesSelected - 1];
+
+                //     //Debug.Log("Displayed Class: " + displayedClassSelection);
+                //     //Debug.Log("Displayed Class: " + displayedInstanceSelection);
+                //     uIInstanceController.cloudClassInstanceSelection(instancesSelected);
+                // }
+                else if (classesSelected == 0 && instancesSelected == 0)
+                {
+                    cloudControllerLLM.ResetSelection();
+                }
+            }  
+        }
+    }
+
+    private void ShowClickedCloud(int cloudClass, int cloudInstance)
+    {
+        // IF CLASS INSTANCE UI HAS NOT BEEN OPENED: OPEN IT AND MARK INSTANCE IN CLASS
+        if (cloudControllerLLM.classInstanceUIActive == false)
+        {
+            cloudControllerLLM.ShowClassInstanceUI(cloudClass);
+            cloudControllerLLM.cloudClassInstanceMouseSelection(cloudClass, cloudInstance);
+            return;
+        }
+
+        // IF CLASS INSTANCE UI IS OPEN: CHECK INSTANCE STATUS
+        // IF INSTANCE SELECTED WHILE OTHER INSTANCE IS ACTIVE
+        else if (cloudControllerLLM.classInstanceUIActive == true && cloudControllerLLM.classInstanceSelected.Contains(true))
+        {
+            // MARK INSTANCE
+            cloudControllerLLM.cloudClassInstanceMouseSelection(cloudControllerLLM.activeClassInstanceInMenu, cloudInstance);
+
+            // IF NO INSTANCES ARE NO LONGER ACTIVE: CLOSE CLASS INSTANCE UI 
+            if (!cloudControllerLLM.classInstanceSelected.Contains(true))
+            {
+                cloudControllerLLM.ShowClassInstanceUI(cloudControllerLLM.activeClassInstanceInMenu);
+            }
+            return;
+        }
+    }
+
+    bool IsPointerOverUI()
+    {
+        return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+    }
+}
+
